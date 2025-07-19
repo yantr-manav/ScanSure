4e92d40f836013207ebc4ff0704863ee788ebe4f
yantr-manav <saivamshijilla04@gmail.com>
2025-07-20 01:06:32 +0530

    Project frontend initialized

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-500 to-indigo-600 text-white">
+      <h1 className="text-4xl font-bold">🚀 ScanSure is Live!</h1>
+    </div>
+  );
+}
+
+export default App;
